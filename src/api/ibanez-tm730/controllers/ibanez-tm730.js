5ef44288525c68521ed70842cf7f7a7ac806451a
Lucas Boilly <boilly.lucas@gmail.com>
2023-04-04 16:03:43 -0300

    Separation of the front and the back

---
+++ src/api/ibanez-tm730/controllers/ibanez-tm730.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * ibanez-tm730 controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::ibanez-tm730.ibanez-tm730');
---
+++ src/api/ibanez-tm730/routes/ibanez-tm730.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * ibanez-tm730 router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::ibanez-tm730.ibanez-tm730');
